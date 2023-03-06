5ddc0d902d5dce873d98205b2eddc0c03dbbdcc4
Sebastian Canales <spideronyou100@gmail.com>
2023-03-06 11:28:55 +0000

    Completed lab start

--- src/main/java/Questions.java
+++ src/main/java/Questions.java
@@ -1,7 +1,3 @@
-/* 
- * Please analyze the Vehicle class in the "Vehicle.java" file and answer the following questions below.
- * Note: The exercise expects you to have read the "Entities of Java" topic in the curriculum.
- */
 public class Questions {
     int question1(){
         /* 
@@ -9,7 +5,7 @@ public class Questions {
          * 
          * How many variables are in the class "Vehicle"?
          */
-        return 0;
+        return 3;
     }
 
     int question2(){
@@ -18,14 +14,13 @@ public class Questions {
          * 
          * What line number represents the end of the "accelerate" method? 
          */
-        return 0;
+        return 8;
     }
 
     String question3(){
         /* 
          * What datatype is expected to be returned in the "honk" method?
          */
-        return "replace this text with your answer";
+        return "String";
     }
-    
 }
